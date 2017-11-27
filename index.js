1c9384870e36ecb7fdffba59e1d3147ee397bdd6
Nestor Zepeda <NestorDZepeda@gmail.com>
2017-11-26 23:29:12 -0800

    Fixed server error

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send(req);
+    res.send({data: "Hello World!"});
 });
 
 const PORT = process.env.PORT || 8000;
