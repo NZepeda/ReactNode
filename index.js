18ebc76d336cfbb1d6d3b4e088c174ef9b0e4890
Nestor Zepeda <NestorDZepeda@gmail.com>
2017-12-04 23:55:27 -0800

    User login and logout implemented

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const cookieSession = require('cookie-session');
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+require('dotenv').config();
+require('./models/User');
+require('./services/passport');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({data: "Hello World!"});
-});
+
+console.log(process.env.cookieKey);
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [process.env.cookieKey]
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+// Mongoose connection
+mongoose.connect(process.env.DB_URL,{useMongoClient: true});
 
 const PORT = process.env.PORT || 8000;
-
 app.listen(PORT, () => {
-    console.log('Listening on port: ', 8000);
+    console.log('Listening on port: ', PORT);
 });
---
+++ services/passport.js
@@ -0,0 +1,39 @@
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('Users'); 
+
+passport.serializeUser((user, done) => {
+    console.log(user);
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        console.log("Deserializing: ", user);
+        done(null, user);
+    })
+}); 
+
+// Configure passport 
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+    User.findOne({googleId: profile.id}).then((existingUser) => {
+        if(existingUser){
+            // User already exists
+            done(null, existingUser);
+        }
+        else{
+
+            // Create the user 
+            new User({clientId: profile.id}).save().then((user)  => {
+                done(null, user);
+            });
+        }
+    })
+}));
